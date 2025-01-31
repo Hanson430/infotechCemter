8ce0d3684f83fd9c5bbe2ba218005b83098f2010
Hanson <corbinsucksatcoding@gmail.com>
2025-01-31 13:48:56 -0500

    Updated Comments and Print Function for Welcome Branch

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,6 +1,3 @@
-# Programmer: Corbin Hanson
-# Branch: Welcome
-# Date: 1.30.2025
-# Program: BlackWidow
+print("Welcome Branch - Developer: Corbin Hanson")
 
 print("Welcome to InfoTech Center V1.0")
