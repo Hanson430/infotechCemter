c483102fc90a2150eb68fe7e2e74684b804fb572
Hanson <corbinsucksatcoding@gmail.com>
2025-02-07 10:48:14 -0500

    Added some Spacing to Code

--- BlackWidow.py
+++ BlackWidow.py
@@ -4,20 +4,26 @@ import time
 
 from google.protobuf.internal.test_bad_identifiers_pb2 import message
 
-print("Welcome Branch - Developer: Corbin Hanson")
+print("\nWelcome Branch - Developer: Corbin Hanson")
 
-print("\n\tWelcome to InfoTech Center V1.0")
+print("\nWelcome to InfoTech Center V1.0\n")
 
 x = 0
 ellipsis = 0
 
 while x != 20:
     x += 1
+
     message = ("Infotech Center System Booting" + "." * ellipsis)
+
     ellipsis += 1
+
     sys.stdout.write("\r" + message)
+
     time.sleep(.5)
+
     if ellipsis == 4:
         ellipsis = 0
+
     if x == 20:
-        print("\nOperating System Booted Up - Retina Scanned - Access Granted")
+        print("\n\nOperating System Booted Up - Retina Scanned - Access Granted")
