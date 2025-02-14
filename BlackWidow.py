51c3bad65b23ab21177890123931c5bf9b260e26
Hanson <corbinsucksatcoding@gmail.com>
2025-02-14 13:57:12 -0500

    Tested gasLevelGauge - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -5,7 +5,7 @@ import random
 from time import sleep
 
 def gasLevelGauge():
-    gasLevelList = ["Empty". "Low", "Quarter Tank", "Half Tank", "Three Quarter Tank", "Full Tank"]
+    gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarter Tank", "Full Tank"]
     return random.choice(gasLevelList)
   
-
+print(gasLevelGauge())
